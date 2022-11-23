85c57bc39b3d4fe9fcef661fac37d912d90120e9
Matheus Macedo <matheusg3.201168@gmail.com>
2022-11-23 04:14:47 -0300

    first commit

---
+++ src/components/button/index.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { ThemeContext, ThemeProvider } from '../../contexts/theme-context'
+
+export const Button = (props)=> {
+    const { theme } = useContext(ThemeContext)
+
+    return(
+        <button {...props}
+            style={{color: theme.color, backgroundColor: theme.background }}
+        />
+    )
+}
+    
+
+    
